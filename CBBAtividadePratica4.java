4fdb821698599d0efee83f88e1ea293241880241
Leticia <92949025+leticiaesttrela@users.noreply.github.com>
2022-10-25 16:42:29 -0300

    Praticando

---
+++ CBBAtividadePratica4.java
@@ -0,0 +1,8 @@
+public class CBBAtividadePratica4 {
+    public static void main(String[] args) {
+        int limite = 100;
+        int i;
+        for (i=0; i<=100; i++)
+        System.out.println(i);
+    }
+}
